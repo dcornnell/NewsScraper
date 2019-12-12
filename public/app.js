962fbdb3bc675ac7997a132119dc9c8945160466
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-12 13:38:30 -0500

    Added a comment form to each story

--- public/app.js
+++ public/app.js
@@ -7,12 +7,19 @@ $(document).ready(function() {
             <a href="#" class="accordion-title"><h5>${data[i].title}</h5></a>
             <div class="accordion-content" data-tab-content>
             <p>${data[i].description}</p>
-            <a href="${data[i].link}">Read more!</a>
+            <a href="${data[i].link}"type="button" class="success button">Read more!</a>
+            <textarea id="${data[i]._id}"></textarea>
+            <a href="#" class="button comment" data='${data[i]._id}'>Leave a comment</a>
             </div>
             </li>`;
 
             $(".accordion").append(item);
         }
         Foundation.reInit("accordion");
+    });
+
+    $(document).on("click", ".comment", function() {
+        console.log($(`textarea#${$(this).attr("data")}`).val());
+        console.log($(this).attr("data"));
     });
 });

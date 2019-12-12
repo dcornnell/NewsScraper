2cd470ba83a33b4bd06555d6a8f75296ac92aeb4
Daniel Cornnell <daniel.cornnell@gmail.com>
2019-12-11 23:06:42 -0500

    added foundation

--- public/app.js
+++ public/app.js
@@ -1,7 +1,18 @@
-$(document).ready(
+$(document).ready(function() {
+    $(document).foundation();
+
     $.get("/all").then(function(data) {
         for (let i = 0; i < data.length; i++) {
-            console.log(data[i]);
+            const item = `<li class="accordion-item" data-accordion-item>
+            <a href="#" class="accordion-title"><h5>${data[i].title}</h5></a>
+            <div class="accordion-content" data-tab-content>
+            <p>${data[i].description}</p>
+            <a href="${data[i].link}">Read more!</a>
+            </div>
+            </li>`;
+
+            $(".accordion").append(item);
         }
-    })
-);
+        Foundation.reInit("accordion");
+    });
+});
